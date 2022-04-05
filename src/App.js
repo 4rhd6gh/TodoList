6b30b29899077e856338b2e72a6981455a58e046
ssun-aa <ssk6200@daum.net>
2022-04-05 14:16:52 +0900

    modify folder name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main.js";
 import Jihyuk from "./pages/jihyuk";
-import Suna from "./pages/suna";
+import Sunah from "./pages/sunah";
 import Byungsu from "./pages/byungsu";
 import Jinho from "./pages/jinho";
 
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/byungsu" element={<Byungsu />} />
         <Route path="/jihuk" element={<Jihyuk />} />
-        <Route path="/suna" element={<Suna />} />
+        <Route path="/sunah" element={<Sunah />} />
         <Route path="/jinho" element={<Jinho />} />
       </Routes>
     </Router>
