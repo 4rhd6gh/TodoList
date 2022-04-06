b40f3e55220fc5ef2de258849201574261859281
ssun-aa <ssk6200@daum.net>
2022-04-07 01:31:51 +0900

    make redux module

---
+++ src/pages/sunah/modules/todos.js
@@ -0,0 +1,70 @@
+const CREATE = "todos/CREATE";
+const REMOVE = "todos/REMOVE";
+const EDIT = "todos/EDIT";
+const TOGGLE = "todos/TOGGLE";
+
+let todoId = 1;
+export const create = (text) => ({
+  type: CREATE,
+  todo: {
+    id: todoId++, // 새 항목을 추가하고 nextId 값에 1을 더해줍니다.
+    text,
+  },
+});
+
+export const remove = (id) => ({
+  type: REMOVE,
+  id,
+});
+
+export const edit = (id, text) => ({
+  type: EDIT,
+  todo: {
+    id,
+    text,
+  },
+});
+
+export const toggle = (id) => ({
+  type: TOGGLE,
+  id,
+});
+
+const initialTodos = [
+  // {
+  //   id: 1,
+  //   text: "일기 쓰기",
+  //   done: true,
+  // },
+  // {
+  //   id: 2,
+  //   text: "밥 먹기",
+  //   done: false,
+  // },
+  // {
+  //   id: 3,
+  //   text: "씻기",
+  //   done: false,
+  // },
+];
+
+export default function todos(state = initialTodos, action) {
+  switch (action.type) {
+    case CREATE:
+      console.log("!!");
+      console.log(state.concat(action.todo));
+      return state.concat(action.todo);
+    case TOGGLE:
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    case REMOVE:
+      return state.filter((todo) => todo.id !== action.id);
+    case EDIT:
+      return state.map((todo) =>
+        todo.id === action.todo.id ? action.todo : todo
+      );
+    default:
+      return state;
+  }
+}
